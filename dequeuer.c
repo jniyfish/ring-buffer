974772603d04ad84ee7950b9d70db02b1975eb2e
minchihAta <minchih@atayalan.com>
2023-11-23 06:26:55 +0000

    remove binary

--- dequeuer.c
+++ dequeuer.c
@@ -1,20 +1,35 @@
 #include "ring.h"
 #include <stdio.h>
+#include <string.h>
 
+void print_help() {
+    printf("insert \"exit\" to exit program\n");
+    printf("insert \"get\" to get a entry\n");
+}
 
 int main()
 {
     queue_t* queue = init_queue(sizeof(entry_t), 32, O_RDWR);
-    printf("entry size: %u, entry num: %u\n", queue->entry_size, queue->entry_num);
-
-    for (int i=0, j=10 ;i <10; i++, j++) {
-        entry_t *entry = NULL;
-        int rt = dequeue(queue, &entry);
-        if (rt >= 0)
-            printf("entry idx: %u, entry value: %u\n", entry->idx, entry->value);
-        print_queue_head_tail(queue);
+    print_help();
+    while(1) {
+        char buf[20];
+        printf("dequeuer> ");
+        scanf("%s", buf);
+        if (strcmp(buf, "exit") == 0) {
+            printf("Exit\n");
+            break;
+        } else if (strcmp(buf, "get") == 0) {
+            entry_t *entry = NULL;
+            int rt = dequeue(queue, &entry);
+            if (rt >= 0)
+                printf("entry idx: %u, entry value: %u\n", entry->idx, entry->value);
+            else 
+                printf("dequeue failed: %d\n", rt);
+            print_queue_head_tail(queue);
+        } else {
+            print_help();
+        }
     }
-    print_queue_head_tail(queue);
 
     return 0;
 }
--- enqueuer.c
+++ enqueuer.c
@@ -1,14 +1,22 @@
 #include "ring.h"
 #include <stdio.h>
-
+#include <string.h>
 
 int main()
 {
     queue_t* queue = init_queue(sizeof(entry_t), 32, SHM_INIT);
-    printf("entry size: %u, entry num: %u\n", queue->entry_size, queue->entry_num);
-    for (int i=0, j=10 ;i <10; i++, j++) {
-        enqueue(queue, i, j);
-        print_queue_head_tail(queue);
+    printf("insert data index-value example: 1-1 \n");
+    while(1) {
+        char buf[20]={0};
+        printf("enqueuer>");
+        scanf("%s", buf);
+        if (strcmp(buf, "exit") == 0) {
+            printf("Exit\n");
+            break;
+        }
+        uint32_t idx, value;
+        sscanf(buf,"%u-%u", &idx, &value);
+        enqueue(queue, idx, value);
     }
 
     print_queue_head_tail(queue);
